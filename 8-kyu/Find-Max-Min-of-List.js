9d513cf9f7e2ec46ed7d845d69614d6e00ba90ed
GeoffrySheehan <medium0fry@gmail.com>
2021-03-12 10:54:39 -0500

    Added url to challenge.

--- 8-kyu/Find-Max-Min-of-List.js
+++ 8-kyu/Find-Max-Min-of-List.js
@@ -1,3 +1,6 @@
+// Find Maximum and Minimum Values of a List
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
 var min = function(list){
   let temp_list = list.map(x => x)
   temp_list.sort((x, y) => Number(x) - Number(y))
@@ -6,6 +9,6 @@ var min = function(list){
 
 var max = function(list){
   let temp_list = list.map(x => x)
-  temp_list.sort((x, y) => Number(x) - Number(y))
-  return temp_list[temp_list.length - 1];
+  temp_list.sort((x, y) => Number(y) - Number(x))
+  return temp_list[0];
 }
