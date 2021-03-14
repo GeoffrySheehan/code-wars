7ea005bdf00ceaa7f0e2617fb59127ebc274d09c
GeoffrySheehan <medium0fry@gmail.com>
2021-03-14 11:49:07 -0400

    Create goose-filter.js

---
+++ 8-kyg/goose-filter.js
@@ -0,0 +1,9 @@
+// Filter out the geese
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+function gooseFilter (birds) {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter(bird => !geese.includes(bird))
+  
+};
