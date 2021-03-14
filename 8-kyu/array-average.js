9c9b984e514ba38478446763b108f5f44e4569e2
GeoffrySheehan <medium0fry@gmail.com>
2021-03-14 11:56:12 -0400

    Create array-average.js

---
+++ 8-kyu/array-average.js
@@ -0,0 +1,5 @@
+// Get the mean of an array
+// https://www.codewars.com/kata/563e320cee5dddcf77000158
+function getAverage(marks){
+  return Math.floor(marks.reduce((sum, x) => sum += x) / marks.length)
+}
