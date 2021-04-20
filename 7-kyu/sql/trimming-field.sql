05c72eb3816f3142f6e4f0ad25a620a36aad55d5
geosheehan <medium0fry@gmail.com>
2021-04-20 14:54:51 -0400

    Create trimming-field.sql

---
+++ 7-kyu/sql/trimming-field.sql
@@ -0,0 +1,9 @@
+-- SQL Basics - Trimming the Field
+-- https://www.codewars.com/kata/59401c25c15cbeb58d000028
+SELECT
+  id
+  , name
+  , SPLIT_PART(characteristics, ',', 1) AS characteristic
+FROM
+  monsters AS m
+ORDER BY id
