e08705125fd61e9acfd9e1fab1eb2a6b8513683f
Geoffry Sheehan <medium0fry@gmail.com>
2021-04-22 08:29:37 -0400

    DUE 4/24/2021 Fake commit to change comment

--- 6-kyu/js/counting-duplicates.js
+++ 6-kyu/js/counting-duplicates.js
@@ -1,6 +1,7 @@
 // Counting Duplicates
 // https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
 
+
 /**
 * This function counts the number of characters that appear more than once in the given string
 * Examples:
