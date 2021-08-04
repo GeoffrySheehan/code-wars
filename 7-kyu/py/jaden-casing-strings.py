d9d6f698f23fec0e86e0be91fc07e691c29a5406
Geoffry Sheehan <medium0fry@gmail.com>
2021-08-04 10:20:34 -0400

    Update jaden-casing-strings.py

--- 7-kyu/py/jaden-casing-strings.py
+++ 7-kyu/py/jaden-casing-strings.py
@@ -1,2 +1,4 @@
+# Jaden Casing Strings
+# https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/python
 def to_jaden_case(string):
     return ' '.join([word.capitalize() for word in string.split(' ')])
